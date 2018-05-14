c181c4980dabc4db7484a5e21560d0c4107e7ebb
Alexandru <alex.ene0x11@gmail.com>
2018-05-15 00:43:20 +0100

    Something that builds and does not crash

--- build.rs
+++ build.rs
@@ -14,24 +14,39 @@ fn main() {
         .expect("Couldn't write bindings!");
 
     let target = env::var("TARGET").unwrap();
-    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let mut lib_dir = manifest_dir.clone();
-    let mut dll_dir = manifest_dir.clone();
-    dll_dir.push("msvc");
-    dll_dir.push("x64");
-    lib_dir.push("msvc");
-    lib_dir.push("x64");
-    println!("cargo:rustc-link-search=all={}", lib_dir.display());
-    //panic!("dlldir {:?}", dll_dir);
-    for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
-        let entry_path = entry.expect("Invalid fs entry").path();
-        let file_name_result = entry_path.file_name();
-        let mut new_file_path = manifest_dir.clone();
-        if let Some(file_name) = file_name_result {
-            let file_name = file_name.to_str().unwrap();
-            if file_name.ends_with(".dll") {
-                new_file_path.push(file_name);
-                std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+    if target.contains("pc-windows") {
+        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        let mut lib_dir = manifest_dir.clone();
+        let mut dll_dir = manifest_dir.clone();
+        if target.contains("msvc") {
+            lib_dir.push("msvc");
+            dll_dir.push("msvc");
+        }
+        else {
+            lib_dir.push("gnu-mingw");
+            dll_dir.push("gnu-mingw");
+        }
+        if target.contains("x86_64") {
+            lib_dir.push("x64");
+            dll_dir.push("x64");
+        }
+        else {
+            lib_dir.push("x86");
+            dll_dir.push("x86");
+        }
+        println!("cargo:rustc-link-search=all={}", lib_dir.display());
+        println!("cargo:rustc-link-lib=dylib=hawktracer");
+        //println!("cargo:rustc-link-search=native=/msvc/x64");
+        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
+            let entry_path = entry.expect("Invalid fs entry").path();
+            let file_name_result = entry_path.file_name();
+            let mut new_file_path = manifest_dir.clone();
+            if let Some(file_name) = file_name_result {
+                let file_name = file_name.to_str().unwrap();
+                if file_name.ends_with(".dll") {
+                    new_file_path.push(file_name);
+                    std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+                }
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::{thread, time};
 extern crate rust_hawktracer;
+use std::os::raw::c_char;
 use rust_hawktracer::*;
 
 fn main() {
     unsafe {
         let c_name = std::ffi::CString::new("BLABLA").unwrap();
+        let p: *mut *mut c_char = std::ptr::null_mut();
+        ht_init(0, p);
+
         ht_feature_callstack_start_string(ht_global_timeline_get(), c_name.as_ptr());
         thread::sleep(time::Duration::from_millis(1000));
         ht_feature_callstack_stop(ht_global_timeline_get());
         ht_timeline_flush(ht_global_timeline_get());
+        ht_deinit();
     }
 }
