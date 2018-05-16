7230e89eba46514a7ef32db996676008ce6aaec6
Alexandru <alex.ene0x11@gmail.com>
2018-05-17 00:59:49 +0100

    scoped_tracepoint! is implemented

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,25 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[macro_export]
+macro_rules! scoped_tracepoint {
+    ($name:ident) => {
+        let tracepoint_name = concat!(stringify!($name), "\0");
+        unsafe {
+            ht_feature_callstack_start_string(ht_global_timeline_get(), tracepoint_name.as_ptr() as _);
+        }
+        let $name = ScopedTracepoint{}
+    };
+}
+
+
+pub struct ScopedTracepoint;
+
+impl Drop for ScopedTracepoint {
+    fn drop(&mut self) {
+        unsafe {
+            ht_feature_callstack_stop(ht_global_timeline_get());
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::{thread, time};
+
+#[macro_use]
 extern crate rust_hawktracer;
 use rust_hawktracer::*;
 use std::os::raw::c_char;
 
+
 fn main() {
-    unsafe {
+    let listener = unsafe {
         let c_name = std::ffi::CString::new("BLABLA").unwrap();
         let p: *mut *mut c_char = std::ptr::null_mut();
         ht_init(0, p);
@@ -25,13 +28,25 @@ fn main() {
             Some(ht_file_dump_listener_callback),
             listener as _,
         );
+        ht_registry_push_all_klass_info_events(ht_global_timeline_get());
 
         ht_feature_callstack_start_string(timeline, c_name.as_ptr());
         thread::sleep(time::Duration::from_millis(10));
         ht_feature_callstack_stop(timeline);
-        ht_timeline_flush(timeline);
+        listener
+    };
 
+
+    {
+        scoped_tracepoint!(test);
+        thread::sleep(time::Duration::from_millis(30));
+    }
+
+    unsafe {
+        ht_timeline_flush(ht_global_timeline_get());
         ht_file_dump_listener_destroy(listener);
         ht_deinit();
     }
 }
+
+
