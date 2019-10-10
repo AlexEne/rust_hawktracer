2ec5a1e68676d38ffb61abe07ad9eb47f4eb6b3a
Alexandru <alex.ene0x11@gmail.com>
2019-10-10 01:12:49 +0100

    proc_macro tracing

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,12 @@
 #[allow(dead_code)]
-mod internals;
+extern crate rust_hawktracer_sys;
+extern crate rust_hawktracer_proc_macro;
+extern crate rust_hawktracer_normal_macro;
 
-#[allow(unused_imports)]
-use crate::internals::hawktracer_listener::HawktracerListener;
+pub use rust_hawktracer_sys::ScopedTracepoint;
+pub use rust_hawktracer_sys::HawktracerListenerType;
+pub use rust_hawktracer_sys::HawktracerInstance;
 
-pub use crate::internals::scoped_tracepoint::ScopedTracepoint;
-pub use crate::internals::hawktracer_instance::HawktracerListenerType;
-pub use crate::internals::hawktracer_instance::HawktracerInstance;
+pub use rust_hawktracer_proc_macro::*;
 
-#[macro_export]
-#[cfg(feature = "profiling_enabled")]
-macro_rules! scoped_tracepoint {
-    ($name:ident) => {
-        let tracepoint_name = concat!(stringify!($name), "\0");
-        ScopedTracepoint::start_trace(tracepoint_name.as_ptr() as _);
-        let $name = ScopedTracepoint {};
-    };
-}
-
-#[macro_export]
-#[cfg(not(feature = "profiling_enabled"))]
-macro_rules! scoped_tracepoint {
-    ($name:ident) => {
-        ()
-    };
-}
+pub use rust_hawktracer_normal_macro::*;
