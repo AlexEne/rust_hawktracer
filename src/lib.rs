73eea5ab14bda437425bf5ef2ac14a56a042b462
Alexandru <alex.ene0x11@gmail.com>
2019-10-10 23:28:30 +0100

    remove rust_hawktracer_sys dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 #[allow(dead_code)]
-extern crate rust_hawktracer_sys;
 extern crate rust_hawktracer_proc_macro;
 extern crate rust_hawktracer_normal_macro;
-
-pub use rust_hawktracer_sys::ScopedTracepoint;
-pub use rust_hawktracer_sys::HawktracerListenerType;
-pub use rust_hawktracer_sys::HawktracerInstance;
 
 pub use rust_hawktracer_proc_macro::*;
 
