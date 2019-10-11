05ded4a197ca26c5e6b911fb440d20706b16d0bc
Alexandru Ene <alex.ene0x11@gmail.com>
2019-10-11 01:40:58 +0100

    Proc macros (#14)
    Added proc_macros & split this project into 4 crates.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,7 @@
 #[allow(dead_code)]
-mod internals;
+extern crate rust_hawktracer_proc_macro;
+extern crate rust_hawktracer_normal_macro;
 
-#[allow(unused_imports)]
-use crate::internals::hawktracer_listener::HawktracerListener;
+pub use rust_hawktracer_proc_macro::*;
 
-pub use crate::internals::scoped_tracepoint::ScopedTracepoint;
-pub use crate::internals::hawktracer_instance::HawktracerListenerType;
-pub use crate::internals::hawktracer_instance::HawktracerInstance;
-
-#[macro_export]
-#[cfg(feature = "profiling_enabled")]
-macro_rules! scoped_tracepoint {
-    ($name:ident) => {
-        let tracepoint_name = concat!(stringify!($name), "\0");
-        ScopedTracepoint::start_trace(tracepoint_name.as_ptr() as _);
-        let $name = ScopedTracepoint {};
-    };
-}
-
-#[macro_export]
-#[cfg(not(feature = "profiling_enabled"))]
-macro_rules! scoped_tracepoint {
-    ($name:ident) => {
-        ()
-    };
-}
+pub use rust_hawktracer_normal_macro::*;
--- tests/utils.rs
+++ tests/utils.rs
@@ -2,6 +2,11 @@ extern crate rust_hawktracer;
 use std::{thread, time};
 use rust_hawktracer::*;
 
+#[hawktracer(interesting)]
+fn method_to_profile() {
+    thread::sleep(time::Duration::from_millis(10));
+}
+
 pub fn do_work() { 
     scoped_tracepoint!(_work);
 
@@ -14,4 +19,6 @@ pub fn do_work() {
         scoped_tracepoint!(_test);
         thread::sleep(time::Duration::from_millis(10));
     }
+
+    method_to_profile();
 }
